fee2c4812fbe481272744c8abd380a0d8f3da9b8
danieljanderson <danieljanderson0616@gmail.com>
2019-07-12 19:28:30 -0400

    created artistschema and albumschema

--- First_tutorial/UpStarMusic/database/models/album.js
+++ First_tutorial/UpStarMusic/database/models/album.js
@@ -1,1 +1,15 @@
-// Todo: create Album Schema
+/* eslint-disable import/newline-after-import */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+module.exports = AlbumSchema;
